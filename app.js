5765170c26137385f9d9928574582d05e82e0736
Sheldon Z Collins <84816623+Sunrisedad@users.noreply.github.com>
2021-09-16 10:44:48 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
+// const pageHTML = generatePage(name, github);
 
-const generatePage = require('./src/page-template.js');
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
 
-const profileDataArgs = process.argv.slice(2);
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
-const [name, github] = profileDataArgs;
-
-
-
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
-
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
